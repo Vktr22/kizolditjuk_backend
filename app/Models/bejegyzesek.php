6f2c840c136c3aebdf1e32804b39f9073052e51e
Vktr22 <nds.viktoria@gmail.com>
2026-04-16 10:34:56 +0200

    mejegyzesek modelben allapot értékekk beállítása, +fillable

--- app/Models/bejegyzesek.php
+++ app/Models/bejegyzesek.php
@@ -10,6 +10,17 @@ class bejegyzesek extends Model
     /** @use HasFactory<\Database\Factories\BejegyzesekFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'tevekenyseg_id',
+        'osztaly_nev',
+        'allapot',
+    ];
+
+    public function getAllapotLabelAttribute(): string
+    {
+        return $this->allapot ? 'Elfogadva' : 'Nincs elfogadva';
+    }
+
     protected function casts(): array
     {
         return [
